39fb7bd56e05de99d409b3744fb64acfcbefb7ed
Rohan Awhad <rawhad@redhat.com>
2025-07-17 15:09:25 -0400

    chore: refactored matcher into a simple function

--- tests/mock_inference_server/app.py
+++ tests/mock_inference_server/app.py
@@ -10,106 +10,14 @@ from werkzeug import exceptions  # type: ignore[import-not-found]
 import click  # type: ignore[import-not-found]
 import yaml
 
-# Matching
-# Standard
-from abc import abstractmethod
-from typing import Protocol
-import pprint
+def match_prompt(prompt: str, patterns: list[dict]) -> str:
+  for p in patterns:
+    if "contains" in p and all(c in prompt for c in p["contains"]):
+      return p["response"]
+    elif "contains" not in p and prompt:
+      return p["response"]
+  return ""
 
-
-class Match(Protocol):
-    """
-    Match represents a single prompt matching
-    strategy. When a match is successful,
-    the response is what should be returned.
-    """
-
-    response: str
-
-    @abstractmethod
-    def match(self, prompt: str) -> str | None:
-        raise NotImplementedError
-
-
-class Always:
-    """
-    Always is a matching strategy that always
-    is a positive match on a given prompt.
-
-    This is best used when only one prompt response
-    is expected.
-    """
-
-    def __init__(self, response: str):
-        self.response = response
-
-    def match(self, prompt: str) -> str | None:
-        if prompt:
-            return self.response
-        return None
-
-
-class Contains:
-    """
-    Contains is a matching strategy that checks
-    if the prompt string contains all of
-    the substrings in the `contains` attribute.
-    """
-
-    contains: list[str]
-
-    def __init__(self, contains: list[str], response: str):
-        if not contains or len(contains) == 0:
-            raise ValueError("contains must not be empty")
-        self.response = response
-        self.contains = contains
-
-    def match(self, prompt: str) -> str | None:
-        if not prompt:
-            return None
-        for context in self.contains:
-            if context not in prompt:
-                return None
-
-        return self.response
-
-
-# helper function pulled out for easier testing
-def to_match(pattern: dict) -> Match:
-    response = pattern.get("response")
-    if not response:
-        raise ValueError(
-            f"matching strategy must have a response: {pprint.pformat(pattern, compact=True)}"
-        )
-    if "contains" in pattern:
-        return Contains(**pattern)
-    return Always(**pattern)
-
-
-class Matcher:
-    """
-    Matcher matches prompt context and then
-    selects a user provided reply.
-    """
-
-    strategies: list[Match]
-
-    def __init__(self, matching_patterns: list[dict]):
-        if not matching_patterns:
-            raise ValueError(
-                "matching strategies must contain at least one Match strategy"
-            )
-
-        self.strategies: list[Match] = []
-        for matching_pattern in matching_patterns:
-            self.strategies.append(to_match(matching_pattern))
-
-    def find_match(self, prompt: str) -> str:
-        for strategy in self.strategies:
-            response = strategy.match(prompt)
-            if response:
-                return response
-        return ""
 
 
 # mock openAI completion responses
@@ -144,7 +52,7 @@ def create_chat_completion(content: str, model: str = "gpt-3.5") -> dict:
 
 # Globals
 app = Flask(__name__)
-strategies: Matcher  # a read only list of matching strategies
+strategies: list[dict]  # a read only list of matching strategies
 
 # Routes
 @app.route("/v1/completions", methods=["POST"])
@@ -162,8 +70,8 @@ def completions():
         f"{request.method} {request.url} {data['model']} {prompt_debug_str}"
     )
 
-    chat_response = strategies.find_match(
-        prompt
+    chat_response = match_prompt(
+        prompt, strategies
     )  # handle prompt and generate correct response
 
     response = create_chat_completion(chat_response, model=data.get("model"))
@@ -204,7 +112,7 @@ def start_server(config):
 
     # create match strategy object
     global strategies  # pylint: disable=global-statement
-    strategies = Matcher(conf.matches)
+    strategies = conf.matches
 
     # init server
     app.run(debug=conf.debug, port=conf.port)
